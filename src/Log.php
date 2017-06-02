a1727a958866fed6595cc246c3993b0987eb5a58
liangyong <liangyong@rrkd.cn>
2017-06-02 12:32:58 +0800

    测试

---
+++ src/Log.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xialiangyong
+ * Date: 2017/6/2
+ * Time: 11:21
+ */
+
+namespace db;
+
+
+class Log
+{
+
+    /**
+     * 打印日志
+     */
+    public static function record($message, $type)
+    {
+
+    }
+}
---
+++ src/exception/ValidateException.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xialiangyong
+ * Date: 2017/6/2
+ * Time: 12:31
+ */
+
+namespace db\exception;
+
+
+class ValidateException extends \RuntimeException
+{
+    protected $error;
+
+    public function __construct($error)
+    {
+        $this->error = $error;
+        $this->message = is_array($error) ? implode("\n\r", $error) : $error;
+    }
+
+    /**
+     * 获取验证错误信息
+     * @access public
+     * @return array|string
+     */
+    public function getError()
+    {
+        return $this->error;
+    }
+}
